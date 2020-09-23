271a2a4cc16c96909f319b3b0854e9b823fd5c58
Matthew Smith <mattrwsmith99@gmail.com>
2020-09-22 22:30:27 -0400

    Added production build of the frontend to the backend server

---
+++ Part3/part3/build/precache-manifest.c51f7a7f0a0c0999da310922f184e35a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0866d858c6910496a08b6bd17ae9635",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ad85a396112565b924b",
+    "url": "/static/css/main.9bbe7c2d.chunk.css"
+  },
+  {
+    "revision": "69c6b86caf87f039ec3a",
+    "url": "/static/js/2.76c752df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.76c752df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ad85a396112565b924b",
+    "url": "/static/js/main.843b6ceb.chunk.js"
+  },
+  {
+    "revision": "4b4367ca4fbadb01e2d9",
+    "url": "/static/js/runtime-main.e5a25448.js"
+  }
+]);
---
+++ Part3/part3/build/precache-manifest.ed1ba99432fa68905282c996531d2168.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "514967eea1333e0d4f540016d3368620",
+    "url": "/index.html"
+  },
+  {
+    "revision": "629f3cad91c3d45a3a0b",
+    "url": "/static/css/main.9bbe7c2d.chunk.css"
+  },
+  {
+    "revision": "69c6b86caf87f039ec3a",
+    "url": "/static/js/2.76c752df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.76c752df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "629f3cad91c3d45a3a0b",
+    "url": "/static/js/main.2a220950.chunk.js"
+  },
+  {
+    "revision": "4b4367ca4fbadb01e2d9",
+    "url": "/static/js/runtime-main.e5a25448.js"
+  }
+]);
